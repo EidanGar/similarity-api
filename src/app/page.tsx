18caedaf52c0f6fdd9f449ca6019414fe8552e7e
EidanGar <eidangarcia787i@gmail.com>
2023-07-11 20:48:30 -0400

    Created home page and implemented most of docs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,27 @@
-import { Inter } from "next/font/google";
-const inter = Inter({ subsets: ["latin"] });
-
-import type { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
 import LargeHeading from "@/components/ui/LargeHeading";
 import Paragraph from "@/components/ui/Paragraph";
-import Link from "next/link";
+
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Similarity API | Home",
-  description: "Free and open-source similarity API.",
+  description: "Free & open-source text similarity API",
 };
 
 export default function Home() {
   return (
-    <div className="realtive h-screen flex items-center justify-center overflow-hidden">
-      <div className="container pt-32 max-w-7xl mx-auto w-full h-full">
+    <div className="relative h-screen flex items-center justify-center overflow-x-hidden">
+      <div className="container pt-32 max-w-7xl w-full mx-auto h-full">
         <div className="h-full gap-6 flex flex-col justify-start lg:justify-center items-center lg:items-start">
           <LargeHeading
-            size={"lg"}
+            size="lg"
             className="three-d text-black dark:text-light-gold"
           >
             Easily determine <br /> text similarity.
           </LargeHeading>
+
           <Paragraph className="max-w-xl lg:text-left">
             With the Text Similarity API, you can easily determine the
             similarity between two pieces of text with a free{" "}
@@ -33,6 +33,18 @@ export default function Home() {
             </Link>
             .
           </Paragraph>
+
+          <div className="relative w-full max-w-xl lg:max-w-3xl lg:left-1/2 aspect-square lg:absolute">
+            <Image
+              priority
+              className="img-shadow "
+              quality={100}
+              style={{ objectFit: "contain" }}
+              fill
+              src="/typewriter.webp"
+              alt="typewriter"
+            />
+          </div>
         </div>
       </div>
     </div>
